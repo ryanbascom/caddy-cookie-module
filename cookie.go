68f30651634f70f18ff975f484b67f3b087ce7b5
ryan.bascom <ryan.bascom@route.com>
2020-06-22 14:52:26 -0600

    Removed ommitempty from struct

--- cookie.go
+++ cookie.go
@@ -22,7 +22,7 @@ func init() {
 type Cookie struct {
 	logger          *zap.Logger
 	Disabled		bool
-	CookiesToRemove []string `json:"cookies_to_remove,omitempty"`
+	CookiesToRemove []string `json:"cookies_to_remove"`
 }
 
 func (c Cookie) CaddyModule() caddy.ModuleInfo {
